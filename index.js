eb258d09415c5c680f20d4eac69f8189354c5dc3
vinicius Fernandes <vinicim.fernandez1@gmail.com>
2021-01-13 16:16:25 -0300

    Adding HTTPS Suport

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 //Primary file for the API
 
-
 //Dependencies
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');//ES6
+var fs = require('fs');
 
-//The server should responde to all request with a string
-var server = http.createServer(function(req,res){
+//Instantiate the http server
+var httpServer = http.createServer(function(req,res){
+    unifiedServer(req,res);
+});
+
+//Start the http server
+httpServer.listen(config.httpPort,function(){
+    console.log("the server is listenig on port:"+config.httpPort+" in mode :"+config.envName);
+});
+
+//Instantiate the https server
+var httpsServerOptions = {
+    'key': fs.readFileSync('./https/key.pem'),
+    'cert': fs.readFileSync('./https/cert.pem')
+};
+
+var httpsServer = https.createServer(httpsServerOptions,function(req,res){
+    unifiedServer(req,res);
+});
+
+//Start the https server
+httpsServer.listen(config.httspPort,function(){
+    console.log("the server is listenig on port:"+config.httpsPort+" in mode :"+config.envName);
+});
+
+//All the server logic for both the http and https server
+var unifiedServer = function(req,res){
     //Get the URL and parse it
     var parsedUrl = url.parse(req.url,true)//ATENÇÃO
 
@@ -66,12 +92,7 @@ var server = http.createServer(function(req,res){
             console.log('Returning this response: ',statusCode,payloadString)
         });
     })
-});
-
-//Start the server
-server.listen(config.port,function(){
-    console.log("the server is listenig on port:"+config.port+" in mode :"+config.envName);
-});
+};
 
 //Define handlers
 var handlers = {};
